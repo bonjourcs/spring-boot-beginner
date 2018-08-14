5552d080170f95880a208660fd96b84766c7b84c
Liang Chenghao <lch_office@foxmail.com>
2018-08-14 14:10:39 +0800

    init: initialize project.

---
+++ src/main/java/com/bonjourcs/java/spring/boot/SpringBootBeginnerApplication.java
@@ -0,0 +1,12 @@
+package com.bonjourcs.java.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBeginnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBeginnerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/bonjourcs/java/spring/boot/SpringBootBeginnerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bonjourcs.java.spring.boot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootBeginnerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
