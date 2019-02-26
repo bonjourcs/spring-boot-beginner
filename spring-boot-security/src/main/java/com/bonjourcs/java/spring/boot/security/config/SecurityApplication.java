7c9c7c4c98e945b53521d2c1ee3d1104cbbbb4cf
Liang Chenghao <lch_office@foxmail.com>
2019-02-26 17:33:53 +0800

    update(security): add main class.

---
+++ spring-boot-security/src/main/java/com/bonjourcs/java/spring/boot/security/config/SecurityApplication.java
@@ -0,0 +1,18 @@
+package com.bonjourcs.java.spring.boot.security.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Liang Chenghao
+ * Description:
+ * Date: 2019/2/26
+ */
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+
+}
