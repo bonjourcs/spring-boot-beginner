26605a8f6b915b7b8bd9ea753705ea983320baaf
Liang Chenghao <lch_office@foxmail.com>
2019-12-27 15:37:38 +0800

    update(web): add spring mapstruct code.

--- spring-boot-web/src/main/java/com/bonjourcs/java/spring/boot/web/model/mapping/PersonMapper.java
+++ spring-boot-web/src/main/java/com/bonjourcs/java/spring/boot/web/model/mapping/PersonMapper.java
@@ -4,14 +4,12 @@ import com.bonjourcs.java.spring.boot.web.model.dto.PersonDto;
 import com.bonjourcs.java.spring.boot.web.model.vo.PersonVo;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.stereotype.Component;
 
 /**
  * @author Liang Chenghao
  * Description:
  * Date: 2019/12/27
  */
-@Component
 @Mapper(componentModel = "spring")
 public interface PersonMapper {
 
