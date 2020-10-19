1bc708a02b620a573b6242ef53d43aa433c02dc3
Liang Chenghao <lch_office@foxmail.com>
2020-10-19 16:28:58 +0800

    update(web): add i18n and logging support.

---
+++ spring-boot-web/src/main/java/com/bonjourcs/java/spring/boot/web/controller/I18NController.java
@@ -0,0 +1,26 @@
+package com.bonjourcs.java.spring.boot.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Liang Chenghao
+ * Description: i18n endpoint
+ * Date: 2020/10/19
+ */
+@RestController
+public class I18NController {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @GetMapping("/i18n")
+    public String getI18NInfo(){
+        return messageSource.getMessage("message.greeting", null, LocaleContextHolder.getLocale());
+    }
+
+
+}
---
+++ spring-boot-web/src/main/java/com/bonjourcs/java/spring/boot/web/interceptor/I18nInterceptor.java
@@ -0,0 +1,36 @@
+package com.bonjourcs.java.spring.boot.web.interceptor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+/**
+ * @author Liang Chenghao
+ * Description:
+ * Date: 2020/10/19
+ */
+@Configuration
+public class I18nInterceptor implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        registry.addInterceptor(interceptor);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.CHINA);
+        return sessionLocaleResolver;
+    }
+
+}
+
